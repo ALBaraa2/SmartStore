66440ac2ce2583da15495b2c4747448140410e25
ALBaraa <albaraawork3@gmail.com>
2025-07-01 17:18:44 +0300

    Enhance checkbox styling for improved user experience and accessibility

--- resources/views/customer/cart/view.blade.php
+++ resources/views/customer/cart/view.blade.php
@@ -30,14 +30,23 @@
                             @php $totalPrice += $subtotal; @endphp
                             <tr class="border-b">
                                 <td class="py-3 px-6 text-center">
-                                    <input 
-                                        type="checkbox" 
-                                        class="pay-now-checkbox w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" 
-                                        data-id="{{ $item->id }}" 
-                                        data-price="{{ $item->product->price }}" 
-                                        data-quantity="{{ $item->quantity }}"
-                                        checked
-                                    >
+                                    <div class="inline-flex items-center">
+                                        <label class="flex items-center cursor-pointer relative">
+                                            <input 
+                                                type="checkbox" 
+                                                class="pay-now-checkbox peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-blue-600 checked:border-blue-600" 
+                                                data-id="{{ $item->id }}" 
+                                                data-price="{{ $item->product->price }}" 
+                                                data-quantity="{{ $item->quantity }}"
+                                                checked
+                                            >
+                                            <span class="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" stroke-width="1">
+                                            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                                            </svg>
+                                            </span>
+                                        </label>
+                                    </div>
                                 </td>
                                 <td class="py-3 px-6">
                                     <div class="flex items-center">
